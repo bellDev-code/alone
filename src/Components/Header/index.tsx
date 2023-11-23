3a9acc4d3bc6d71a3fc282c0b16270efeae7bf50
bellDev-code <sumaoo20@naver.com>
2023-11-23 14:18:54 +0900

    헤더 영역 추가

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-between p-5'>
+      <div>게임 설명</div>
+      {/* 추후 개발 */}
+      <div>카카오톡 공유</div>
+    </div>
   )
 }
 
