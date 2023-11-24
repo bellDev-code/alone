fd8eecad44cd7773e1a5915c7e83fa4f4b80cbce
bellDev-code <sumaoo20@naver.com>
2023-11-24 15:09:52 +0900

    플레이어 추가 및 View 페이지 구현

--- src/Pages/SinglePage/index.tsx
+++ src/Pages/SinglePage/index.tsx
@@ -3,17 +3,49 @@ import usePlayerStore from '@Store/usePlayerStore'
 import React from 'react'
 
 const SinglePage = () => {
-  const { username, setUsername } = usePlayerStore();
+  const { username, setUsername, addPlayers, players } = usePlayerStore();
 
-  const onChangeName = () => {
-    setUsername(username)
+  const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value)
+  }
+
+  const handleEnterKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && event.nativeEvent.isComposing === false) {
+      event.preventDefault();
+      addNames();
+    }
+  };
+
+  const addNames = () => {
+    if(username.length > 1) {
+      addPlayers()
+    }
   }
 
   return (
-    <div className='flex items-center justify-center'>
-      <div className='flex gap-2'>
-        <input type="text" placeholder='이름을 입력해주세요' value={username} onChange={onChangeName} />
-        <BaseButton>추가</BaseButton>
+    <div className='px-5'>
+      <div className='flex flex-col items-center justify-center'>
+        <div className='flex gap-2'>
+          <input type="text" placeholder='이름을 입력해주세요' 
+            value={username} 
+            onChange={onChangeName} 
+            onKeyDown={handleEnterKey} 
+          />
+          <BaseButton onClick={addNames}>추가</BaseButton>
+        </div>
+        <div className='h-[120px] w-3/5 my-5 overflow-auto'>
+          {players.map((player, index) => {
+            return (
+              <div className='py-1' key={index}>{index + 1}. {player.username}</div>
+            )
+          })}
+        </div>
+      </div>
+      <div className='flex'>
+        <div className='text-lg'>총 인원 : {players.length}</div>
+      </div>
+      <div className='flex justify-end'>
+        <BaseButton>다음</BaseButton>
       </div>
     </div>
   )
--- src/Store/usePlayerStore.ts
+++ src/Store/usePlayerStore.ts
@@ -3,11 +3,24 @@ import { create } from 'zustand';
 type singleInputState = {
     username: string;
     setUsername: (name: string) => void;
+    players: {username: string}[];
+    addPlayers: () => void;
 }
 
 const usePlayerStore = create<singleInputState>((set) => ({
     username: '',
     setUsername: (name) => set({username: name}),
+    players: [],
+    addPlayers: () => {
+        set((state) => {
+            if(state.username) {
+                const updateNames = [...state.players, { username: state.username }]
+
+                return { players: updateNames, username: ''}
+            }
+            return state
+        })
+    }
 }))
 
 export default usePlayerStore
